3d4ad7cfde3a619e642282532f1da6b74ae377c9
Henry Li <henry1943@163.com>
2022-05-09 19:15:45 +0800

    feat: add appendNewActivityToDay

--- src/core/biz/trip-day.ts
+++ src/core/biz/trip-day.ts
@@ -1,6 +1,7 @@
 import immer from 'immer';
 
 import type { TripDay, Trip, Activity } from '../types';
+import { uuid } from '../util/uuid';
 
 export function getDayIndex(day: TripDay, trip: Trip) {
   return trip.days.findIndex((d) => d.id === day.id);
@@ -11,6 +12,22 @@ export function updateActivityOfDay(activity: Activity, day: TripDay) {
     const activityIndex = day.activities.findIndex((a) => a.id === activity.id);
     if (activityIndex >= 0) {
       draft.activities[activityIndex] = activity;
+    }
+  });
+}
+
+export function appendNewActivityToDay(day: TripDay) {
+  return immer(day, (draft) => {
+    const newActivity: Activity = { id: uuid() };
+    draft.activities.push(newActivity);
+  });
+}
+
+export function removeActivityFromDay(activityId: string, day: TripDay) {
+  return immer(day, (draft) => {
+    const activityIndex = day.activities.findIndex((a) => a.id === activityId);
+    if (activityIndex >= 0) {
+      draft.activities.splice(activityIndex, 1);
     }
   });
 }
