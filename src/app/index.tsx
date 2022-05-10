11b71b2bbb0f2a35800da807cb2f358eb2dc5ad1
Henry Li <henry1943@163.com>
2022-05-10 14:32:23 +0800

    feat: remove 'StrictMode', and add 'tw-app'

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,7 +2,6 @@ import { ConfigProvider } from 'antd';
 import antdLocale from 'antd/lib/locale/zh_CN';
 import moment from 'moment';
 import 'moment/dist/locale/zh-cn';
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
@@ -21,20 +20,26 @@ async function main() {
   // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
   const root = createRoot(document.querySelector('body > #tw-app-root')!);
   root.render(
-    <StrictMode>
-      <ConfigProvider locale={antdLocale}>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/">
-              <Route path="trips">
-                <Route path=":tripId" element={<TripDetailPage />} />
-              </Route>
-            </Route>
-          </Routes>
-        </BrowserRouter>
-      </ConfigProvider>
-    </StrictMode>,
+    <ConfigProvider locale={antdLocale}>
+      <App />
+    </ConfigProvider>,
   );
 }
+
+const App = () => {
+  return (
+    <div className="tw-app">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">
+            <Route path="trips">
+              <Route path=":tripId" element={<TripDetailPage />} />
+            </Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+};
 
 main();
