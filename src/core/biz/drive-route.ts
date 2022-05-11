9ef545aae461ad345de184cb7e0c44e359d59a9a
Henry Li <henry1943@163.com>
2022-05-11 13:09:59 +0800

    feat: add drive-route

---
+++ src/core/biz/drive-route.ts
@@ -0,0 +1,66 @@
+import immer from 'immer';
+
+import { NavigationService } from '../geo';
+import type { DriveRoute, Trip, TripDay } from '../types';
+
+import { getPOIsOfDay } from './poi';
+import { getDayIndex } from './trip-day';
+
+export async function updateRoutesBasedOnChanges(
+  day: TripDay,
+  trip: Trip,
+  callback: (changedTrip: Trip) => void | Promise<void>,
+) {
+  const dayIndex = getDayIndex(day, trip);
+  const possiblyImpactedDays = trip.days.slice(dayIndex);
+  for (const possiblyImpactDay of possiblyImpactedDays) {
+    const possiblyImpactDayIndex = getDayIndex(possiblyImpactDay, trip);
+    let needsUpdate = false;
+    const pois = getPOIsOfDay(possiblyImpactDay, trip);
+    if (pois.length > 1) {
+      if (possiblyImpactDay.route) {
+        const routeId = pois.map((poi) => poi.id).join(',');
+        if (routeId !== possiblyImpactDay.route?.id) {
+          needsUpdate = true;
+        }
+      } else {
+        needsUpdate = true;
+      }
+    } else {
+      if (possiblyImpactDay.route) {
+        needsUpdate = true;
+      }
+    }
+    if (needsUpdate) {
+      console.info(
+        'Needs update',
+        pois.map((poi) => poi.name),
+      );
+      let newRoute: DriveRoute | null = null;
+      if (pois.length > 1) {
+        const routes = await NavigationService.search(pois);
+        if (routes.length) {
+          newRoute = routes[0];
+        }
+      }
+      const changedTrip = immer(trip, (draft) => {
+        draft.days[possiblyImpactDayIndex].route = newRoute;
+      });
+      await callback(changedTrip);
+      await updateRoutesBasedOnChanges(
+        possiblyImpactDay,
+        changedTrip,
+        callback,
+      );
+      break;
+    }
+  }
+}
+
+export function combinePathOfRoute(route: DriveRoute) {
+  const path: AMap.LngLat[] = [];
+  route.steps.forEach((step) => {
+    path.push(...step.path);
+  });
+  return path;
+}
--- src/core/biz/index.ts
+++ src/core/biz/index.ts
@@ -1,4 +1,5 @@
 export * from './activity';
+export * from './drive-route';
 export * from './poi';
 export * from './trip';
 export * from './trip-day';
