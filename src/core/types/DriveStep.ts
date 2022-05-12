0ba3d0087c3d8aa8dc02ed4ec0b443dc5e8c7e4d
Henry Li <henry1943@163.com>
2022-05-12 10:16:05 +0800

    feat: add action and assistant_action

--- src/core/types/DriveStep.ts
+++ src/core/types/DriveStep.ts
@@ -1,7 +1,8 @@
 export interface DriveStep {
   start_location: AMap.LngLat;
   end_location: AMap.LngLat;
-  assist_action: string;
+  action: string;
+  assistant_action: string;
   distance: number;
   time: number;
   path: AMap.LngLat[];
